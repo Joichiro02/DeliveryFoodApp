64e1ac2266123f1ca307f4337cd7c25a7b2deac2
markanthonyfamarin <markanthonyfamarin@gmail.com>
2023-06-03 08:03:19 +0800

    add the basket icon component

---
+++ src/components/BasketIcon.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+
+import Currency from "react-currency-formatter";
+import { useNavigation } from "@react-navigation/native";
+
+import { selectBasketItems, selectBasketTotal } from "features/basketSlice";
+
+// ** redux method
+import { useSelector } from "react-redux";
+
+export default function BasketIcon() {
+    // ** navigation method
+    const navigation = useNavigation();
+
+    // ** basketSlice helper
+    const item = useSelector(selectBasketItems);
+    const basketTotal = useSelector(selectBasketTotal);
+
+    return (
+        <View className="absolute bottom-10 w-full z-50">
+            <TouchableOpacity className="mx-5 bg-[#00CCBB] p-4 rounded-lg flex-row items-center space-x-1">
+                <Text className="text-white font-extrabold text-lg bg-[#01A296] py-1 px-2">
+                    {item.length}
+                </Text>
+                <Text className="flex-1 text-white font-extrabold text-lg text-center">
+                    View Basket
+                </Text>
+                <Text className="text-lg text-white font-extrabold">
+                    <Currency quantity={basketTotal} currency="PHP" />
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+}
--- src/screens/Restaurant.tsx
+++ src/screens/Restaurant.tsx
@@ -3,102 +3,126 @@ import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 // ** libraries
 import { useNavigation, useRoute } from "@react-navigation/native";
 import {
-  ArrowLeftIcon,
-  ChevronRightIcon,
-  MapPinIcon,
-  StarIcon,
+    ArrowLeftIcon,
+    ChevronRightIcon,
+    MapPinIcon,
+    StarIcon,
 } from "react-native-heroicons/solid";
 
 import { urlFor } from "../../delivery-food-app/sanity";
 import { QuestionMarkCircleIcon } from "react-native-heroicons/outline";
 import DishRow from "components/DishRow";
+import BasketIcon from "components/BasketIcon";
 
 export default function Restaurant() {
-  const navigation = useNavigation();
-  const {
-    params: {
-      id,
-      imgUrl,
-      title,
-      rating,
-      genre,
-      address,
-      short_description,
-      dishes,
-      long,
-      lat,
-    },
-  } = useRoute<any>();
+    const navigation = useNavigation();
+    const {
+        params: {
+            id,
+            imgUrl,
+            title,
+            rating,
+            genre,
+            address,
+            short_description,
+            dishes,
+            long,
+            lat,
+        },
+    } = useRoute<any>();
 
-  useLayoutEffect(() => {
-    navigation.setOptions({ headerShown: false });
-  }, []);
+    useLayoutEffect(() => {
+        navigation.setOptions({ headerShown: false });
+    }, []);
 
-  console.log("@@", dishes);
+    console.log("@@", dishes);
 
-  return (
-    <ScrollView>
-      <View className="relative">
-        <Image
-          source={{ uri: urlFor(imgUrl).url() }}
-          className="w-full h-56 bg-gray-300"
-        />
-        <TouchableOpacity
-          onPress={navigation.goBack}
-          className="absolute top-10 left-5 p-2 bg-gray-100 rounded-full"
-        >
-          <ArrowLeftIcon size={20} color="#00CCBB" />
-        </TouchableOpacity>
-      </View>
-      <View className="bg-white">
-        <View className="px-4 pt-4">
-          <Text className="text-3xl font-bold">{title}</Text>
-          <View className="flex-row space-x-2 my-1">
-            <View className="flex-row items-center space-x-1">
-              <StarIcon color="green" opacity={0.5} size={22} />
-              <Text className="text-xs text-gray-500">
-                <Text className="text-green-500">{rating}</Text> · {genre}
-              </Text>
-            </View>
+    return (
+        <>
+            <BasketIcon />
+            <ScrollView>
+                <View className="relative">
+                    <Image
+                        source={{ uri: urlFor(imgUrl).url() }}
+                        className="w-full h-56 bg-gray-300"
+                    />
+                    <TouchableOpacity
+                        onPress={navigation.goBack}
+                        className="absolute top-10 left-5 p-2 bg-gray-100 rounded-full"
+                    >
+                        <ArrowLeftIcon size={20} color="#00CCBB" />
+                    </TouchableOpacity>
+                </View>
+                <View className="bg-white">
+                    <View className="px-4 pt-4">
+                        <Text className="text-3xl font-bold">{title}</Text>
+                        <View className="flex-row space-x-2 my-1">
+                            <View className="flex-row items-center space-x-1">
+                                <StarIcon
+                                    color="green"
+                                    opacity={0.5}
+                                    size={22}
+                                />
+                                <Text className="text-xs text-gray-500">
+                                    <Text className="text-green-500">
+                                        {rating}
+                                    </Text>{" "}
+                                    · {genre}
+                                </Text>
+                            </View>
 
-            <View className="flex-row items-center space-x-1 w-56">
-              <MapPinIcon color="gray" opacity={0.4} size={22} />
-              <Text className="text-xs text-gray-500" numberOfLines={1}>
-                Nearby · {address}
-              </Text>
-            </View>
-          </View>
-          <Text
-            className="text-gray-500 mt-2 pb-4 text-justify"
-            numberOfLines={3}
-          >
-            {short_description}
-          </Text>
-        </View>
-        <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
-          <QuestionMarkCircleIcon color="gray" opacity={0.6} size={20} />
-          <Text className="pl-2 flex-1 text-md font-bold">
-            Have a food allergy?
-          </Text>
-          <ChevronRightIcon color="#00CCBB" />
-        </TouchableOpacity>
-      </View>
+                            <View className="flex-row items-center space-x-1 w-56">
+                                <MapPinIcon
+                                    color="gray"
+                                    opacity={0.4}
+                                    size={22}
+                                />
+                                <Text
+                                    className="text-xs text-gray-500"
+                                    numberOfLines={1}
+                                >
+                                    Nearby · {address}
+                                </Text>
+                            </View>
+                        </View>
+                        <Text
+                            className="text-gray-500 mt-2 pb-4 text-justify"
+                            numberOfLines={3}
+                        >
+                            {short_description}
+                        </Text>
+                    </View>
+                    <TouchableOpacity className="flex-row items-center space-x-2 p-4 border-y border-gray-300">
+                        <QuestionMarkCircleIcon
+                            color="gray"
+                            opacity={0.6}
+                            size={20}
+                        />
+                        <Text className="pl-2 flex-1 text-md font-bold">
+                            Have a food allergy?
+                        </Text>
+                        <ChevronRightIcon color="#00CCBB" />
+                    </TouchableOpacity>
+                </View>
 
-      <View>
-        <Text className="px-4 pt-6 mb-3 font-bold text-xl">Menu</Text>
+                <View className="pb-36">
+                    <Text className="px-4 pt-6 mb-3 font-bold text-xl">
+                        Menu
+                    </Text>
 
-        {/* Dishrows */}
-        {dishes?.map((item) => (
-          <DishRow
-            key={item._id}
-            id={item._id}
-            name={item.name}
-            short_description={item.short_description}
-            price={item.price}
-            image={item.image}
-          />
-        ))}
-      </View>
-    </ScrollView>
-  );
+                    {/* Dishrows */}
+                    {dishes?.map((item) => (
+                        <DishRow
+                            key={item._id}
+                            id={item._id}
+                            name={item.name}
+                            short_description={item.short_description}
+                            price={item.price}
+                            image={item.image}
+                        />
+                    ))}
+                </View>
+            </ScrollView>
+        </>
+    );
 }
